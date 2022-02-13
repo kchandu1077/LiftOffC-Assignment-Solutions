d3566495ed416a872146acf18aa57b1a6720ef3a
kchandu1077 <99469873+kchandu1077@users.noreply.github.com>
2022-02-13 08:09:21 +0530

    Add files via upload

---
+++ A2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x,y;
+    printf("Enter first and second number: \n");
+    scanf("%d%d",&x,&y);
+    if(x>y)
+    {
+        printf("%d is greater than %d",x,y);
+    }
+    else if(x<y)
+    {
+        printf("%d is greater than %d",y,x);
+    }
+    else if(x==y)
+    {
+        printf("Both numbers are equal.");
+    }
+    return 0;
+}
